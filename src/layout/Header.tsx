a3ba88b55942a6fe6f325a3e47957d2155ff9369
Vladislav <104913853+Adwasar@users.noreply.github.com>
2024-02-29 19:06:26 +0200

    feat: header layout

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,4 +1,7 @@
-import searchImgPath from '../assets/icons/search.svg'
+import imgPathSearch from '../assets/icons/search.svg'
+import imgPathUser from '../assets/icons/user.svg'
+import imgPathLiked from '../assets/icons/liked.svg'
+import imgPathCard from '../assets/icons/card.svg'
 
 function Header() {
   return (
@@ -6,17 +9,28 @@ function Header() {
       <div className="header">
         <div className="container">
           <div className="header__content">
-            <div>LOGO</div>
+            <div className="header__logo">LOGO</div>
+            <div className="burger">
+              <span className="burger__tt"></span>
+              <span className="burger__mm"></span>
+              <span className="burger__bb"></span>
+            </div>
             <form className="search-form">
               <button className="search-form__btn">
-                <img className="search-form__btn-icon" src={searchImgPath} alt="" />
+                <img className="search-form__btn-icon" src={imgPathSearch} alt="Search" />
               </button>
               <input className="search-form__input" type="text" placeholder="пошук товарів" />
             </form>
             <div className="header__actions">
-              <button className="header__user"></button>
-              <button className="header__favorites"></button>
-              <button className="header__card"></button>
+              <button className="header__btn-user">
+                <img src={imgPathUser} alt="User" />
+              </button>
+              <button className="header__btn-liked">
+                <img src={imgPathLiked} alt="Heard" />
+              </button>
+              <button className="header__btn-card">
+                <img src={imgPathCard} alt="Card" />
+              </button>
             </div>
           </div>
         </div>
